cb1442475dc164263b2cde1c677b5c7d9bc645ff
Luigi Pirelli <luipir@gmail.com>
2021-04-17 16:31:44 +0200

    generate server in the current tree

--- gencode.sh
+++ gencode.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
-docker run --rm -u "$(id -u):$(id -g)" -v ${PWD}:/local -v${PWD}/../../python-flask-server/:/out   openapitools/openapi-generator:cli-v5.1.0 generate -i /local/swagger.yaml -g python-flask -o /out
+docker run --rm -u "$(id -u):$(id -g)" -v ${PWD}:/local openapitools/openapi-generator:cli-v5.1.0 generate -i /local/swagger.yaml -g python-flask -o /local/python-flask-server/
 if [ $? -eq 1 ]; then
 	exit
 fi
