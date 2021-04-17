cec9a429671c455b11048073d3edd5bb9d0a50ef
Luigi Pirelli <luipir@gmail.com>
2021-04-17 16:54:05 +0200

    modified tags

--- python-flask-server/openapi_server/controllers/search_controller.py
+++ python-flask-server/openapi_server/controllers/search_controller.py
@@ -2,16 +2,71 @@ import connexion
 import six
 
 from openapi_server.models.area import Area  # noqa: E501
+from openapi_server.models.props import Props  # noqa: E501
 from openapi_server import util
 
 
-def delete_are_by_name(name):  # noqa: E501
-    """Delete a Area resource by name.
+def get_area_by_date(date):  # noqa: E501
+    """Retrieve a Area with a specified data.
 
-    Delete a Area resource by name.  # noqa: E501
+    Retrieve a Area resource by date.  # noqa: E501
+
+    :param date: Get all resources containing the name
+    :type date: str
+
+    :rtype: List[Area]
+    """
+    return 'do some magic!'
+
+
+def get_area_by_name(name):  # noqa: E501
+    """Retrieve a Area resource by unique name.
+
+    Retrieve a Area resource by unique name.  # noqa: E501
 
     :param name: resource unique name
     :type name: str
+
+    :rtype: List[Area]
+    """
+    return 'do some magic!'
+
+
+def get_area_by_properties(props=None):  # noqa: E501
+    """Retrieve a Area with a specified data set of properties.
+
+    Retrieve a Area resource by properties dict.  # noqa: E501
+
+    :param props: Area to retrieve with matching properties
+    :type props: dict | bytes
+
+    :rtype: List[Area]
+    """
+    if connexion.request.is_json:
+        props = Props.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
+
+
+def get_intersected(request_body=None):  # noqa: E501
+    """Retrieve a Area inteersecting posted polygon.
+
+    Retrieve intersecting Area resources   # noqa: E501
+
+    :param request_body: Polygon to retrieve intersecting Area resources
+    :type request_body: List[]
+
+    :rtype: List[Area]
+    """
+    return 'do some magic!'
+
+
+def get_intersection(request_body=None):  # noqa: E501
+    """Retrieve Area inside input polygon.
+
+    Retrieve Area inside input polygon  # noqa: E501
+
+    :param request_body: Polygon to retrieve inner Area resources
+    :type request_body: List[]
 
     :rtype: List[Area]
     """
