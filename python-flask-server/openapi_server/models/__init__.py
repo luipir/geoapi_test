87532088a5adf1458ac7c59d0b9ca5763e243df0
Luigi Pirelli <luipir@gmail.com>
2021-04-18 13:10:42 +0200

    removed Props schema no more necessary

--- python-flask-server/openapi_server/models/__init__.py
+++ python-flask-server/openapi_server/models/__init__.py
@@ -5,4 +5,3 @@ from __future__ import absolute_import
 # import models into model package
 from openapi_server.models.area import Area
 from openapi_server.models.point3_d_dict import Point3DDict
-from openapi_server.models.props import Props
